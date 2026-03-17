a0fd10f682142ed73aaf865d1725c7b5551a0728
Daniele Bruni <danielebruni147@gmail.com>
2026-03-17 15:46:03 +0100

    Publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,48 @@
 plugins {
     java
     id("com.gradleup.shadow") version "9.4.0" apply false
+    id("maven-publish")
 }
 
 group = "it.danielebruni.wildadventure"
-version = "1.0.0"
+version = "1.0.1"
+val publishableModules = setOf("WildCommons-Core")
 
 
 subprojects {
+
+    group = rootProject.group
+    version = rootProject.version
+
     apply(plugin = "java")
+    if (name in publishableModules) {
+        apply(plugin = "maven-publish")
+
+        configure<PublishingExtension> {
+            publications {
+                create<MavenPublication>("gpr") {
+                    from(components["java"])
+                    groupId = project.group.toString()
+                    artifactId = project.name.lowercase()
+                    version = project.version.toString()
+                }
+            }
+
+            repositories {
+                maven {
+                    name = "WildCommons"
+                    url = uri("https://maven.pkg.github.com/danib150/WildCommons")
+
+                    credentials {
+                        username = (findProperty("gpr.user") as String?)
+                            ?: System.getenv("GITHUB_ACTOR")
+                        password = (findProperty("gpr.key") as String?)
+                            ?: System.getenv("GITHUB_TOKEN")
+                    }
+                }
+            }
+        }
+    }
 
     group = rootProject.group
     version = rootProject.version
@@ -50,4 +84,6 @@ subprojects {
     tasks.withType<Test>().configureEach {
         useJUnitPlatform()
     }
+
+
 }
